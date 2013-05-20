5b70e0e9858308733b43bfdc15b30c6071849ff0
SimonWu <wuhanyu@gmail.com>
2013-05-20 15:25:50 +0800

    fasdfd

---
+++ superpk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superpk::Application.config.secret_token = '70a1ea47aea8f4826b9c6cc9f997231d0431a7ca79b8ea787b24b9e0faef1e07f0575ced11f305755184a70a1cbc4cdc4cadcf196410174b35dc68bed14d91c2'
